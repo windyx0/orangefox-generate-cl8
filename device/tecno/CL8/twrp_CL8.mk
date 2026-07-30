784f77cbb36e2a14da8d2624b4ead86a935d54ac
Windyx <windyxeditor@gmail.com>
2026-07-30 10:26:35 +0300

    Change product inheritance from Omni to TWRP

--- device/tecno/CL8/twrp_CL8.mk
+++ device/tecno/CL8/twrp_CL8.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from CL8 device
 $(call inherit-product, device/tecno/CL8/device.mk)
